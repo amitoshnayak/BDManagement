a3e28e8d95c7cc8a99f602e299074e8d6ab1e018
amitoshnayak <amitoshnayak049@gmail.com>
2022-08-15 21:17:04 +0530

    blood bank project

---
+++ layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b>Made By: Amitosh Nayak</b>
+            <br>
+            &COPY; BDManagement
+</footer>
+
+
+
+</body>
+</html>
---
+++ users/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: Amitosh Nayak</b>
+            <br>
+            &COPY; BDManagement
+</footer>
+
+
+
+</body>
+</html>
